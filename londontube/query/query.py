cef01fc6b24ec3cde080f36a3849f2a975425a12
dan <xwj.harry@gmail.com>
2023-12-09 15:57:18 +0000

    feat: add func to get the indexes and names of certain stations

--- londontube/query/query.py
+++ londontube/query/query.py
@@ -2,11 +2,12 @@ from londontube.network import Network
 import requests
 import csv
 from io import StringIO
+import pandas as pd
 
 
 def connectivity_of_line(line_index):
     """
-    Query the web service for information about a particular line, and 
+    Query the web service for information about a particular line, and
     contruct a Network object that represent this line.
 
     Parameters
@@ -19,10 +20,12 @@ def connectivity_of_line(line_index):
     Network
         Network of a line.
     """
-    query_total_info = f"https://rse-with-python.arc.ucl.ac.uk/londontube-service/index/query"
+    query_total_info = (
+        f"https://rse-with-python.arc.ucl.ac.uk/londontube-service/index/query"
+    )
     response = requests.get(query_total_info)
     total_info = response.json()
-    
+
     query_web = f"https://rse-with-python.arc.ucl.ac.uk/londontube-service/line/query?line_identifier={line_index}"
     response = requests.get(query_web).content.decode("utf-8")
     # Store line csv information
@@ -39,7 +42,6 @@ def connectivity_of_line(line_index):
 
     line_network = Network(int(total_info["n_stations"]), list_of_edges)
     return line_network
-
 
 
 def disruption_info(date=None):
@@ -69,19 +71,26 @@ def disruption_info(date=None):
     return disruption_info
 
 
+def query_station_to_index(list_of_stations):
+    """query the web service to get the index of each of the stations
 
-def network_of_given_day(date):
+    list_of_stations: List[str]: str is the name of station
+
+    return: List[int],List[str]: the index of the given stations and the name of the given stations
     """
-    Retrieve the whole information of the given day and construct a Network object from this.
 
-    Parameters
-    ----------
-    date : str
-        Given day.
+    if list_of_stations == []:
+        response = requests.get(
+            "https://rse-with-python.arc.ucl.ac.uk/londontube-service/stations/query?id=all"
+        )
+    else:
+        list_str = ",".join(map(str, list_of_stations))
+        requests_str = (
+            "https://rse-with-python.arc.ucl.ac.uk/londontube-service/stations/query?id="
+            + list_str
+        )
+        response = requests.get(requests_str)
+    df = pd.read_csv(StringIO(response.text))
+    return df["station index"].tolist(), df["station name"].tolist()
 
-    Returns
-    -------
-    Network
-        Network representation of londontube.
-    """
-    pass
+
