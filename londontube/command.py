90871acd0f1ef80f41b7b045358f8f37ba23dccb
lazabanma <136291858+lazabanma@users.noreply.github.com>
2023-12-09 14:42:49 +0000

    feat: draft command line interface

--- londontube/command.py
+++ londontube/command.py
@@ -1,7 +1,24 @@
+from argparse import ArgumentParser
+from londontube.query.query import network_of_given_day
 
+def process_station(string: str):
+    if string.isdigit():
+        return int(string)
+    else:
+        return string
+        
 
 def process():
-    pass
+    parser = ArgumentParser(description="journey planner")
+    
+    parser.add_argument('--plot', action="store_true")
+    parser.add_argument('start', type=process_station)
+    parser.add_argument('destination',type=process_station)
+    parser.add_argument('setoff-date', nargs='?')
+    
+    arguments = parser.parse_args()
+    
+    print(arguments)
 
 if __name__ == "__main__":
     process()
