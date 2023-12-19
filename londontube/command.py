f3fc3b15029cdb3ab8f406cb453d5ff4850fd04b
lazabanma <136291858+lazabanma@users.noreply.github.com>
2023-12-19 23:47:06 +0000

    feat: now able to save a plot

--- londontube/command.py
+++ londontube/command.py
@@ -1,7 +1,8 @@
 from _ast import arguments
 from argparse import ArgumentParser
-from londontube.query.query import network_of_given_day, query_station_to_index
+from londontube.query.query import network_of_given_day, query_station_info
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 def process_station(string: str):
     if string.isdigit():
@@ -28,19 +29,30 @@ def process():
 
     start, destination = arguments.start, arguments.destination
     if type(arguments.start) == str:
-        [start], _ = query_station_to_index([arguments.start])
+        [start], _, _, _ = query_station_info([arguments.start])
 
     if type(arguments.destination) == str:
-        [destination], _ = query_station_to_index([arguments.destination])
+        [destination], _, _, _ = query_station_info([arguments.destination])
 
     network = network_of_given_day(current_date)
     path = network.dijkstra(start, destination)
-    path_name = query_station_to_index(path)
+    path_name = query_station_info(path)
     output = "Start: "
     for station in path_name[:len(path_name) - 1]:
         output += station + "\n"
     output += "End: " + path_name[len(path_name) - 1] + "\n"
-    print(output)
+
+    if arguments.plot:
+        all_stations_index, all_stations_name, all_stations_lat, all_stations_long = query_station_info([])
+        plt.scatter(all_stations_lat, all_stations_long)
+        plt.title('Simple Scatter Plot')
+        plt.xlabel('X Axis')
+        plt.ylabel('Y Axis')
+
+        file_string = "journey_from_" + arguments.start + "_to_" + arguments.destination + ".png"
+        plt.savefig(file_string)
+
+
 
 
 
